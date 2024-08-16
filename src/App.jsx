605be9062deb6bd3689c2a482f08e430d32f6106
Mykolas Perevicius <mykolas.perevicius@projectinnovatenewark.org>
2024-08-16 15:14:57 -0400

    Working routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import ShowCreators from './pages/ShowCreators';
 import ViewCreator from './pages/ViewCreator';
 import EditCreator from './pages/EditCreator';
@@ -7,14 +7,14 @@ import AddCreator from './pages/AddCreator';
 
 function App() {
   return (
-    <Router>
+    <div className="App">
       <Routes>
         <Route path="/" element={<ShowCreators />} />
         <Route path="/view/:id" element={<ViewCreator />} />
         <Route path="/edit/:id" element={<EditCreator />} />
         <Route path="/add" element={<AddCreator />} />
       </Routes>
-    </Router>
+    </div>
   );
 }
 
